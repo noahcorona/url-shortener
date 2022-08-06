16c9aae89c26031129f4365c634b6857fd20c27c
noahcorona <noahcorona@gmail.com>
2022-08-06 15:47:50 -0700

    cors headers

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -14,6 +14,11 @@ router.get('/go/:ext', async (req, res) => {
 
       console.log('found', url)
 
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+      res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+
       // redirect the user
       return res.redirect(url.destination);
     } else {
