b55eca8be79eda48486b9f389096212a21d3f8a8
noahcorona <noahcorona@gmail.com>
2022-08-06 15:52:12 -0700

    cors headers

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,21 +3,17 @@ const router = Express.Router();
 const urlDB = require('../models/URL');
 
 router.get('/go/:ext', async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  
   try {
-    console.log(req.params.ext);
     const url = await urlDB.findOne({ ext: req.params.ext });
-    console.log(url);
     if (url) {
       // increment click counter and save database
       url.clicks++;
       url.save();
-
-      console.log('found', url)
-
-      res.setHeader("Access-Control-Allow-Origin", "*");
-      res.setHeader("Access-Control-Allow-Credentials", "true");
-      res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-      res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
       // redirect the user
       return res.redirect(url.destination);
